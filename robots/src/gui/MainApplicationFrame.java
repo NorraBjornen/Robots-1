5a1e9f34ecc8b3fd840337a0ea8efeed3c27ef25
Alex <alexmur20000@mail.ru>
2020-04-28 17:26:33 +0500

    Третье задание готово

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -9,9 +9,11 @@ import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 
+import gui.internal.frames.CoordinatesWindow;
 import gui.internal.frames.GameWindow;
 import gui.internal.frames.LogWindow;
 import log.Logger;
+import logic.robot.Robot;
 import logic.state.DiskStateSaver;
 import gui.menu.MenuBuilder;
 import logic.state.WindowState;
@@ -26,9 +28,10 @@ public class MainApplicationFrame extends JFrame {
 
     private final LogWindow logWindow;
     private final GameWindow gameWindow;
+    private final CoordinatesWindow coordinatesWindow;
     private final DiskStateSaver diskStateSaver;
 
-    public MainApplicationFrame() {
+    public MainApplicationFrame(Robot robot) {
         int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
@@ -37,8 +40,11 @@ public class MainApplicationFrame extends JFrame {
 
         setContentPane(desktopPane);
 
+        gameWindow = createGameWindow(robot);
         logWindow = createLogWindow();
-        gameWindow = createGameWindow();
+        coordinatesWindow = createCoordinatesWindow();
+
+        robot.subscribe(coordinatesWindow);
 
         diskStateSaver = new DiskStateSaver();
         boolean hasSavedData = diskStateSaver.loadFromDisk();
@@ -46,13 +52,16 @@ public class MainApplicationFrame extends JFrame {
         if(hasSavedData) {
             WindowState logState = diskStateSaver.getStateByTag("log");
             WindowState gameState = diskStateSaver.getStateByTag("game");
+            WindowState coordinatesState = diskStateSaver.getStateByTag("coord");
 
             logWindow.setState(logState);
             gameWindow.setState(gameState);
+            coordinatesWindow.setState(coordinatesState);
         }
 
         addWindow(logWindow);
         addWindow(gameWindow);
+        addWindow(coordinatesWindow);
 
         setJMenuBar(new MenuBuilder(this).generateMenuBar());
         addWindowListener(new DialogOnCloseAdapter(this));
@@ -67,6 +76,7 @@ public class MainApplicationFrame extends JFrame {
     public void onClose() {
         diskStateSaver.addState("log", logWindow.getState())
                 .addState("game", gameWindow.getState())
+                .addState("coord", coordinatesWindow.getState())
                 .save();
     }
 
@@ -86,10 +96,20 @@ public class MainApplicationFrame extends JFrame {
     /**
      * Создание окна GameWindow с указанными параметрами по умолчанию
      */
-    private static GameWindow createGameWindow() {
-        GameWindow gameWindow = new GameWindow();
+    private static GameWindow createGameWindow(Robot robot) {
+        GameWindow gameWindow = new GameWindow(robot);
         gameWindow.setSize(400, 400);
         return gameWindow;
+    }
+
+    /**
+     * Создание окна CoordinatesWindow с указанными параметрами по умолчанию
+     */
+    private static CoordinatesWindow createCoordinatesWindow() {
+        CoordinatesWindow coordinatesWindow = new CoordinatesWindow();
+        coordinatesWindow.setLocation(10, 100);
+        coordinatesWindow.setSize(400, 400);
+        return coordinatesWindow;
     }
 
     private void addWindow(JInternalFrame frame) {
---
+++ robots/src/utils/GeometryUtils.java
@@ -0,0 +1,110 @@
+package utils;
+
+import java.util.function.Function;
+
+/**
+ * Utility class для работы с геометрией робота
+ */
+public class GeometryUtils {
+
+    /**
+     * Рассчитывает угол в полярных координатах от робота до цели
+     * и нормализует результат в зависимости от текущего направления робота
+     */
+    public static double angleTo(double fromX, double fromY, double toX, double toY, double direction)
+    {
+        double diffX = toX - fromX;
+        double diffY = toY - fromY;
+
+        double angle = Math.atan2(diffY, diffX);
+
+        if(direction > angle) {
+            while (Math.abs(direction - angle) >= Math.PI) {
+                angle += 2 * Math.PI;
+            }
+        } else if (direction < angle) {
+            while (Math.abs(direction - angle) >= Math.PI) {
+                angle -= 2 * Math.PI;
+            }
+        }
+
+        return angle;
+    }
+
+    /**
+     * Нормализует угол
+     */
+    public static double asNormalizedRadians(double angle)
+    {
+        while (angle < 0)
+        {
+            angle += 2*Math.PI;
+        }
+        while (angle >= 2*Math.PI)
+        {
+            angle -= 2*Math.PI;
+        }
+        return angle;
+    }
+
+    /**
+     * Возвращает расстояние от точки (x1, y1) до точки (x2, y2)
+     */
+    public static double distance(double x1, double y1, double x2, double y2)
+    {
+        double diffX = x1 - x2;
+        double diffY = y1 - y2;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+
+    /**
+     * Рассчитывает угловую скорость робота в зависимости от угла до цели, направления робота и максимальной угловой скорости
+     */
+    public static double calculateAngularVelocity(double angleToTarget, double direction, double maxAngularVelocity) {
+        double velocity = 0;
+        if (angleToTarget > direction) {
+            velocity = maxAngularVelocity;
+        } else if (angleToTarget < direction) {
+            velocity = -maxAngularVelocity;
+        }
+        return velocity;
+    }
+
+    /**
+     * Рассчитывает новую координату X для робота при атомарной операции движения, т.е. Robot.move
+     */
+    public static double calculateX(double x, double velocity, double angularVelocity, double duration, double direction) {
+        double newX = x + calculateDiff(velocity, angularVelocity, duration, direction, Math::sin);
+        if (!Double.isFinite(newX)) {
+            newX = x + calculateNonFiniteDiff(velocity, duration, direction, Math::cos);
+        }
+        return newX;
+    }
+
+    /**
+     * Рассчитывает новую координату Y для робота при атомарной операции движения, т.е. Robot.move
+     */
+    public static double calculateY(double y, double velocity, double angularVelocity, double duration, double direction) {
+        double newY = y - calculateDiff(velocity, angularVelocity, duration, direction, Math::cos);
+        if (!Double.isFinite(newY)) {
+            newY = y + calculateNonFiniteDiff(velocity, duration, direction, Math::sin);
+        }
+        return newY;
+    }
+
+    /**
+     * Рассчитывает расстояние для атомарной операции движения, т.е. Robot.move
+     */
+    private static double calculateDiff(double velocity, double angularVelocity, double duration, double direction, Function<Double, Double> function) {
+        return velocity / angularVelocity *
+                (function.apply(direction + angularVelocity * duration) - function.apply(direction));
+    }
+
+    /**
+     * Рассчитывает расстояние для атомарной операции движения, т.е. Robot.move, в том случае,
+     * если ранее рассчитанная разница не являлась конечной
+     */
+    private static double calculateNonFiniteDiff(double velocity, double duration, double direction, Function<Double, Double> function) {
+        return velocity * duration * function.apply(direction);
+    }
+}
